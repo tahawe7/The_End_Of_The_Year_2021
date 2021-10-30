ecae6cb77e5bbdfadf4c700835e627cc02894f53
TAHAWE <amer.zp582@gmail.com>
2021-10-30 10:02:33 +0200

    Created By Me And Elzero Web School

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// The End Of The Year Date Countdown
+let countDownDate = new Date("Dec 31, 2021 23:59:59").getTime();
+
+// Find The Date Difference Between Date Now And CountDown Date
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Difference Between Now And CountDown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
